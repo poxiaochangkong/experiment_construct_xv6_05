5d37db7ec83ca39bd062d897f66e0763ea43eb72
359 <2023302111359@whu.edu.cn>
2025-09-29 15:52:05 +0800

    避免清零bss段

--- kernel/boot/start.c
+++ kernel/boot/start.c
@@ -7,21 +7,21 @@ extern char ebss[];
 __attribute__ ((aligned (16))) char stack0[4096 * 1];
 
 // entry.S jumps here in machine mode on stack0.
-
+//不要清零，否则stack0可能会被清零
 // 一个简单的 memset 函数实现
-void
-memset(void *dst, int c, unsigned int n)
-{
-  char *cdst = (char *) dst;
-  for (unsigned int i = 0; i < n; i++){
-    cdst[i] = c;
-  }
-}
+// void
+// memset(void *dst, int c, unsigned int n)
+// {
+//   char *cdst = (char *) dst;
+//   for (unsigned int i = 0; i < n; i++){
+//     cdst[i] = c;
+//   }
+// }
 void
 start()
 {
   // 在进入main函数前，清零BSS段
-  memset(sbss, 1, ebss - sbss);
+  //memset(sbss, 1, ebss - sbss);
  
   main();
 
