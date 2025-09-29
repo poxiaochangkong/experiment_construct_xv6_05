8b023750cc32634ae4b05d1e7eea89eb15148526
359 <2023302111359@whu.edu.cn>
2025-09-29 16:23:32 +0800

    memset声明恢复，未来需要移动到别的文件中

--- kernel/boot/start.c
+++ kernel/boot/start.c
@@ -9,14 +9,14 @@ __attribute__ ((aligned (16))) char stack0[4096 * 1];
 // entry.S jumps here in machine mode on stack0.
 //不要清零，否则stack0可能会被清零，并且BSS段似乎提前被qemu清零了
 // 一个简单的 memset 函数实现
-// void
-// memset(void *dst, int c, unsigned int n)
-// {
-//   char *cdst = (char *) dst;
-//   for (unsigned int i = 0; i < n; i++){
-//     cdst[i] = c;
-//   }
-// }
+void
+memset(void *dst, int c, unsigned int n)
+{
+  char *cdst = (char *) dst;
+  for (unsigned int i = 0; i < n; i++){
+    cdst[i] = c;
+  }
+}
 void
 start()
 {
