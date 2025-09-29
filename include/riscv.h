7c7ce7b9430c8842294c673e0bb274214d7c0784
359 <2023302111359@whu.edu.cn>
2025-09-29 15:30:04 +0800

    修改makefile，实现物理页的分配

--- include/riscv.h
+++ include/riscv.h
@@ -1,2 +1,8 @@
 #define KERNBASE 0x80000000L
 #define PHYSTOP (KERNBASE + 128*1024*1024) // 128MB,pyhsical memory limit
+
+#define PGSIZE 4096 // bytes per page
+#define PGSHIFT 12  // bits of offset within a page
+
+#define PGROUNDUP(sz)  (((sz)+PGSIZE-1) & ~(PGSIZE-1))
+#define PGROUNDDOWN(a) (((a)) & ~(PGSIZE-1))
