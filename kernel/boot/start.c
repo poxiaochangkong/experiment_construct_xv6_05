505815f75cd7660d276cfaff8dd4012cbaf9b9ca
359 <2023302111359@whu.edu.cn>
2025-09-29 16:00:05 +0800

    start.c不负责BSS段清零

--- kernel/boot/start.c
+++ kernel/boot/start.c
@@ -7,7 +7,7 @@ extern char ebss[];
 __attribute__ ((aligned (16))) char stack0[4096 * 1];
 
 // entry.S jumps here in machine mode on stack0.
-//不要清零，否则stack0可能会被清零
+//不要清零，否则stack0可能会被清零，并且BSS段似乎提前被qemu清零了
 // 一个简单的 memset 函数实现
 // void
 // memset(void *dst, int c, unsigned int n)
